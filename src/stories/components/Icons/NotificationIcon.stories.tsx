81c194a8fe3233c65bb6618236543aedb127e1f3
LBB <202780@jnu.ac.kr>
2024-11-16 14:51:42 +0900

    Refactor : NotificationIcon 컴포넌트 createSvgIcon, IconButton 사용하는 코드로 리팩토링

--- src/stories/components/Icons/NotificationIcon.stories.tsx
+++ src/stories/components/Icons/NotificationIcon.stories.tsx
@@ -1,5 +1,5 @@
 import { Meta, StoryObj } from '@storybook/react';
-import { NotificationIcon } from '../../../components/Icons/NotificationIcon';
+import NotificationIcon from '@/components/Icons/NotificationIcon';
 
 const meta: Meta = {
     title: 'Icons/NotificationIcon',
@@ -12,10 +12,6 @@ const meta: Meta = {
         },
     },
     argTypes: {
-        size: {
-            control: { type: 'number', min: 10, max: 100, step: 1 },
-            description: '아이콘 크기 설정',
-        },
         onNotificationClick: {
             description: '아이콘을 클릭했을 때 실행되는 콜백 함수',
         },
@@ -28,7 +24,6 @@ type Story = StoryObj<typeof meta>;
 
 export const Primary: Story = {
     args: {
-        size: 16,
         onNotificationClick: () => {
             alert('알림창으로 연결됩니다.');
         },
